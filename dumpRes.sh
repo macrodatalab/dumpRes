02c5241b293e617fecc29180d489021ac3fa999d
yceugenelai <eugene@macrodatalab.com>
2015-07-17 11:59:47 +0800

    Update dumpRes.sh

--- dumpRes.sh
+++ dumpRes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo $#
+#echo $#
 if [ $# -lt "4" ]; then	
 	echo "dumpRes <bo host> <po port> <statement> <dump type> <dump file>"
 	exit 0
@@ -9,16 +9,21 @@ bo_url="http://$1:$2/cmd"
 
 file_name=""
 if [ -z "$5" ]; then
-   file_name="STDOUT"
+   if [ "$4" = "XLSX" ]; then
+	echo "please assign a XLSX file name"
+	exit 0
+   else
+   	file_name="STDOUT"
+   fi
 else
    file_name=$5
 fi
 
-echo "run the script : python borestful.py \"$bo_url\" \"$3\" | python bojson2file.py $4 \"$file_name\""
+echo "run the script : python borestful.py \"$bo_url\" \"$3\" | python bojson2file.py $4 $file_name"
 
 date1=$(date +"%s")
 
-python borestful.py "$bo_url" "$3" | python bojson2file.py $4 "$file_name"
+python borestful.py "$bo_url" "$3" | python bojson2file.py $4 $file_name
 
 date2=$(date +"%s")
 diff=$(($date2-$date1))
