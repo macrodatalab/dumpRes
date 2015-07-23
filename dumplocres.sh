3bef751b9e1e2fe1c3d330714534a76d70205c84
eugene <eugene@macrodatalab.com>
2015-07-23 15:29:12 +0800

    add localscript

---
+++ dumplocres.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#echo $#
+if [ $# -lt "2" ]; then	
+	echo "dumpRes <statement> <dump type> <dump file>"
+	exit 0
+fi
+
+bo_url="http://localhost:9090/cmd"
+
+file_name=""
+if [ -z "$3" ]; then
+   if [ "$2" = "XLSX" ]; then
+	echo "please assign a XLSX file name"
+	exit 0
+   else
+   	file_name="STDOUT"
+   fi
+else
+   file_name=$3
+fi
+
+echo "run the script : python borestful.py \"$bo_url\" \"$1\" | python bojson2file.py $2 $file_name"
+
+date1=$(date +"%s")
+
+python borestful.py "$bo_url" "$1" | python bojson2file.py $2 $file_name
+
+date2=$(date +"%s")
+diff=$(($date2-$date1))
+echo "$diff seconds elapsed."
